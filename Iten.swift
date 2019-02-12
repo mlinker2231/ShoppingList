e41f2acff8b44263da70be6214c94a25e1199b38
Michael Linker <mlinker2231@jh-mba158-08.local>
2019-02-12 11:43:37 -0600

    cdzfgdf

---
+++ Iten.swift
@@ -0,0 +1,16 @@
+//
+//  Iten.swift
+//  ShoppingList
+//
+//  Created by Michael Linker on 2/12/19.
+//  Copyright © 2019 Michael Linker. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -13,20 +13,25 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var NewItemText: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    var items: [Item] = []
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        var item1 = Item.init(name: "Milk")
+        var item2 = Item.init(name: "Eggs")
+        items = [item1,item2]
     }
     @IBAction func AddNewItem(_ sender: Any) {
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
-        cell.textLabel?.text = "Hello"
+        cell.textLabel?.text = items[count].name
+        count += 1
         return cell
     }
-
 }
 
