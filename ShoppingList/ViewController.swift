5f6e6fd67d49b91db433cb8cd105d1c5e19542d9
Michael Linker <mlinker2231@jh-mba158-08.local>
2019-02-12 11:07:59 -0600

    added table view' and data helllos

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -9,13 +9,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var NewItemText: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
-
+    @IBAction func AddNewItem(_ sender: Any) {
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 
 }
 
