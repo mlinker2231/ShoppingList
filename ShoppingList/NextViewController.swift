f5de24cd4dbfa0276bbde685e988fb973539f506
Michael Linker <mlinker2231@jh-mba158-08.d214.org>
2019-02-14 11:10:18 -0600

    Other things

---
+++ ShoppingList/NextViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NextViewController.swift
+//  ShoppingList
+//
+//  Created by Michael Linker on 2/13/19.
+//  Copyright © 2019 Michael Linker. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -10,7 +10,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-
+    
     @IBOutlet weak var NewItemText: UITextField!
     @IBOutlet weak var tableView: UITableView!
     var items: [Item] = []
@@ -18,20 +18,34 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-        var item1 = Item.init(name: "Milk")
-        var item2 = Item.init(name: "Eggs")
+        var item1 = Item.init(name: "Milk", quantity: 3)
+        var item2 = Item.init(name: "Eggs", quantity: 12)
         items = [item1,item2]
+        var item3 = Item.init(name: "Bread", quantity: 7)
+        items.append(item3)
     }
     @IBAction func AddNewItem(_ sender: Any) {
+        if let enteredText = NewItemText.text, enteredText != "" {
+            items.append(createNewItem(NameOfItem: enteredText,Quantity: 1))
+            tableView.reloadData()
+        }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
-        cell.textLabel?.text = items[count].name
-        count += 1
-        return cell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") {
+            let currentItemName = items[indexPath.row].name
+            cell.textLabel?.text = currentItemName
+            count += 1
+            return cell
+        }else {
+            return UITableViewCell()
+        }
+    }
+    func createNewItem(NameOfItem name: String,Quantity: Int) -> Item{
+        var newItem = Item.init(name: name,quantity: Quantity)
+        return newItem
     }
 }
 
